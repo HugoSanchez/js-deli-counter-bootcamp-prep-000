3cba34c6ae468beaaa4e987e025ad70410526cf7
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-06 10:54:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
+  
   katzDeliLine.push(name);
+  
   return "Welcome," + name + ". You are number " + katzDeliLine.indexOf(name)+1 + " in line.";
   
-  //var position = katzDeli["name"] + 1
-  //return "Welcome, ${name}. You are number ${position} in the line."
 }
