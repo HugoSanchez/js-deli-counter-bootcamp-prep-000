d08e29a4cbc554a7d05a5f6b824c9742a640b8fd
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-06 11:02:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   var position = parseInt(katzDeliLine.indexOf(name))+1
   
-  return "Welcome," + name + ". You are number " + position + " in line.";
+  return "Welcome, " + name + ". You are number " + position + " in line.";
   
 }
